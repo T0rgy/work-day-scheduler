1115b7d191e896e280f23d1d0f1093e18e345281
Torgy <jctorge@gmail.com>
2022-04-01 18:27:21 -0500

    save btn work

--- assets/script.js
+++ assets/script.js
@@ -3,43 +3,53 @@
 var currentDate = moment().format('llll')
 $("#currentDay").append(currentDate);
 
-// view timeblock
+// timeblock var's
 var hourTime = moment().startOf("day").add(7, "hour");
 var hour = moment().format("H");
 
-for (var i = 0; i < 9; i++) {
-    var hourBlock = hourTime.add(1, "hour").format("HH:mm");
-    var blockColor;
+// view timeblock
+function taskScheduler() {
+    // 8 hour array
+    for (var i = 0; i < 9; i++) {
+        var hourBlock = hourTime.add(1, "hour").format("HH:mm");
+        var blockColor;
 
-    if (hour == i) {
-        blockColor = 'present';
-    } else if (hour > i) {
-        blockColor = "past";
-    } else if (hour < i) {
-        blockColor = 'future';
-    }
+        // to decide if blockColor
+        if (hour == i) {
+            blockColor = 'present';
+        } else if (hour > i) {
+            blockColor = "past";
+        } else if (hour < i) {
+            blockColor = 'future';
+        } 
 
-    var blockInsert =
-        `<container class="row" id='hour-${i}'>
-            <div class="col-2"></div>
-            <div class="hour w-25 p-4 col-1">${hourBlock}</div>
-            <textarea class="description w-50 p-4 col-6 ${blockColor} hour-${i}"></textarea>
-            <button class="saveBtn w-25 p-4 col-1 fas fa-save fa-2x"></button>
-            <div class="col-2">
-            </div>  
-        </container>
-        <p></p>`
-        ;
+        // hour block html code
+        var blockInsert =
+            `<container class="row" id='hour-${i}'>
+                <div class="col-1"></div>
+                <div class="hour w-25 p-4 col-2">${hourBlock}</div>
+                <textarea id="" class="description w-50 p-4 col-7 ${blockColor} hour-${i}"></textarea>
+                <button id="" class="saveBtn w-25 p-4 col-2 fas fa-save fa-2x"></button>
+                <div class="col-2">
+                </div>  
+            </container>
+            <p></p>`
+            ;
 
-    $("#container").append(blockInsert);
+        //insert block into div container
+        $("#container").append(blockInsert);
+    };
+
+    // click button save
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, text);
+    });
+
 };
 
-$(".saveBtn").on("click", function() {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr('id');
-    JSON.parse(localStorage.setItem(time, text));
-})
 
-// for (var i = 8; i <9; i++) {
-//     $(`.hour-${i}`).val(localStorage.getItem(`hour-${i}`));
-// }
+
+taskScheduler();
+
